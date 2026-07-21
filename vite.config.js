32b39f8725bf41ffe5e08de448e1a37d211059f3
krishna joshi <115784862+krishna-joshi177@users.noreply.github.com>
2026-07-21 19:50:59 +0530

    Adjust Vercel build config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/',
   plugins: [react()],
-  build: {
-    outDir: 'docs',
-  },
 });
